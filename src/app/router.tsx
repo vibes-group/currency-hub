40d900f68091c5879364350cfff8baab7a5d10f7
Nikita M <iamlegkiy@gmail.com>
2026-05-27 16:41:15 +0200

    remove intl

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,41 +1,17 @@
-import { Navigate, Outlet, Route, Routes, useParams } from 'react-router';
+import { Route, Routes } from 'react-router';
 
-import { I18nProvider } from '@/i18n/provider';
-import { isLocale, routing, type Locale } from '@/i18n/routing';
 import { DemoPage } from '@/screens/demo';
 import { HomePage } from '@/screens/home';
 import { ListPage } from '@/screens/list';
 
-function LocaleRoute() {
-  const params = useParams();
-
-  if (!isLocale(params.locale)) {
-    return <Navigate to={`/${routing.defaultLocale}`} replace />;
-  }
-
-  return (
-    <I18nProvider locale={params.locale as Locale}>
-      <Outlet />
-    </I18nProvider>
-  );
-}
-
 export function AppRouter() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Navigate to={`/${routing.defaultLocale}`} replace />}
-      />
-      <Route path="/:locale" element={<LocaleRoute />}>
-        <Route index element={<HomePage />} />
-        <Route path="demo" element={<DemoPage />} />
-        <Route path="list" element={<ListPage />} />
-      </Route>
-      <Route
-        path="*"
-        element={<Navigate to={`/${routing.defaultLocale}`} replace />}
-      />
-    </Routes>
+    <I18nProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/demo" element={<DemoPage />} />
+        <Route path="/list" element={<ListPage />} />
+      </Routes>
+    </I18nProvider>
   );
 }
--- src/features/change-locale/ui/locale-toggle.tsx
+++ src/features/change-locale/ui/locale-toggle.tsx
@@ -1,15 +1,8 @@
 import { useTransition } from "react";
-
-import { useLocale, useTranslations } from "@/i18n/provider";
-import { routing, type Locale } from "@/i18n/routing";
-import { usePathname, useRouter } from "@/i18n/navigation";
 import { LanguagesIcon } from "@/shared/icons";
 import { ToggleGroup, ToggleGroupItem } from "@/shared/ui";
 
 export function LocaleToggle() {
-  const locale = useLocale() as Locale;
-  const pathname = usePathname();
-  const router = useRouter();
   const t = useTranslations("LocaleToggle");
   const [isPending, startTransition] = useTransition();
 
